733e74900aa33eb8af8dc015d3d864b163dc564c
rafaelitajahy <rafaelitajahy@gmail.com>
2022-02-08 18:21:08 -0300

    variables, expressions e instructions

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Hello')
+
+# Operadores aritméticos
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 ** 2)
+
+print(3 ^ 2) # operadores bitwise - https://wiki.python.org/moin/BitwiseOperators
+
+
+# Valores e tipos
+
+print(type(2))
+print(type(2.5))
+print(type('2'))
---
+++ variables_expressions_instructions.py
@@ -0,0 +1,40 @@
+# variables
+
+message = 'Hey! Whats up?'
+num = 23
+pi = 3.1415
+
+# Nomes de varáveis
+"""
+    * Letras minúsculas separadas por _
+    * Nome de classes maiúsculas
+    * Não é permitido:
+        * começar com número
+        * caracteres especiais
+        * espaços
+        * palavras reservadas:
+            O Python 3 tem estas palavras-chave:
+            and     del     from    None        True
+            as      elif    global  nonlocal    try
+            assert  else    if      not         while
+            break   except  import  or          with
+            class   False   in      pass        yield
+            continue        finally is          raise
+            def     for     lambda  return
+"""
+
+# Ordem das operações
+
+"""
+Parênteses >  Exponenciação > Multiplicação e a Divisão > Adição e a Subtração > esquerda para a direita
+"""
+
+# Operações com strings
+
+name = ' Rafael '
+last_name = 'Itajahy'
+full_name = name + last_name
+print(full_name)
+
+repet = 'GO '*5
+print(repet)
